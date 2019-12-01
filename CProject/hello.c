44c2c29b659ccaf2847a2362c1301c4735cf4e33
peng.xing <southeast_px@163.com>
2019-12-01 18:20:03 +0800

    c语言标准库的使用！

--- CProject/hello.c
+++ CProject/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 void main() {
 	printf("hello c!\n");
-	printf("北京\t上海\t天津");
-	getchar();
+	printf("北京\t上海\t天津\n");
+	double result = pow(2.0, 3.0);
+	printf("%.2f", result);
+	//getchar();
+	system("pause");
 }
