d6a289b8e4a0a949608ddb76fb51199e13c80dfc
peng.xing <southeast_px@163.com>
2019-12-01 21:58:31 +0800

    c语言中变量！

--- CProject/hello.c
+++ CProject/hello.c
@@ -5,7 +5,12 @@ void main() {
 	printf("hello c!\n");
 	printf("北京\t上海\t天津\n");
 	double result = pow(2.0, 3.0);
-	printf("%.2f", result);
+	printf("%.2f \n", result);
+	int num = 1;
+	double score = 2.3;
+	char gender = 'A';
+	char name[] = "pengx";
+	printf("num=%d score=%.2f gender=%c name=%s \n", num, score, gender, name);
 	//getchar();
 	system("pause");
 }
