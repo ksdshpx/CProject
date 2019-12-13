ca1210409f4c9fef4aea3e02d0dd815ade85794b
ksdshpx <ksdshpx2018@aliyun.com>
2019-12-13 15:41:56 +0800

    C语言中的字符类型！
    C语言中的字符类型！

--- CProject/hello.c
+++ CProject/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+//宏定义
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
 void main() {
 	printf("hello c!\n");
 	printf("北京\t上海\t天津\n");
@@ -11,6 +16,15 @@ void main() {
 	char gender = 'A';
 	char name[] = "pengx";
 	printf("num=%d score=%.2f gender=%c name=%s \n", num, score, gender, name);
+	printf("int类型的长度=%d", sizeof(int));
 	//getchar();
+	int isPass = 1;
+	if (isPass) {//C语言中0表示假，非0表示真
+		printf("通过考试！");
+	}
+	BOOL isOk = TRUE;
+	if (isOk) {
+		printf("ok!");
+	}
 	system("pause");
 }
